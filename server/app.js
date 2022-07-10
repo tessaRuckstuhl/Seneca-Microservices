19a930e15a91ff1ea19b4613ad1551ef3843cf69
tessaRuckstuhl <tessa.ruckstuhl@web.de>
2022-07-11 00:30:09 +0200

    fix

--- server/app.js
+++ server/app.js
@@ -71,7 +71,7 @@ router.route("/exercise3/:name/:productId").get(function (req, res) {
 });
 
 router.get("/exercise4", function (req, res) {
-  res.json({ message: "group 9 application deployed using kubernetes" });
+  res.send({ message: "group 9 application deployed using kubernetes" });
 });
 /**
  * REGISTER OUR ROUTES
